df0b08428facab36eb2da0579d5791e27374e458
gimlim1 <ricardojosepineda@hotmail.com>
2015-12-17 19:32:13 -0600

    WCF

---
+++ ProyectoTopicos/AdventureWorks.BL/Especificaciones/ConsultasOrderHeader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventureWorks.EF.GR.Repositorios;
+
+namespace AdventureWorks.BL.Especificaciones
+{
+    public class ConsultasOrderHeader
+    {
+        public Boolean ConsultarPorID(int ID)
+        {
+            RepOrderHeader Consulta = new RepOrderHeader();
+            bool bandera = false;
+            var listaOrderHeader = Consulta.ConsultaPorClienteID(ID).ToList();
+            int i = listaOrderHeader.Count();
+            if (i!=0)
+            {
+                bandera = true;
+            }
+
+            return bandera;
+        }
+    }
+}
